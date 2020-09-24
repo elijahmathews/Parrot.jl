3888d98e4910cd1717bfc04005264de6af009bd7
Elijah Mathews <pgp@elijahmathews.com>
2020-09-24 13:14:59 -0500

    readd sitename

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ push!(LOAD_PATH, "../src/")
 
 using Documenter, ProspectorML
 
-makedocs()
+makedocs(sitename="ProspectorML.jl")
 
 deploydocs(
     repo = "github.com/elijahmathews/ProspectorML.jl.git"
