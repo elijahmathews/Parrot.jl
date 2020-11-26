916df46cbc371c1a2ffba1aad2d9cd0289c05222
Elijah Mathews <pgp@elijahmathews.com>
2020-11-26 17:01:43 -0600

    update code documentation

--- src/layers.jl
+++ src/layers.jl
@@ -1,5 +1,5 @@
 # 
-# Parrot.jl/src/neuralnet.jl
+# Parrot.jl/src/layers.jl
 # 
 # By Elijah Mathews (me@elijahmathews.com)
 # openpgp4fpr:ac1d3fb1e8a5eb7d14bd587b2932c725055a90d8
--- src/prospect.jl
+++ src/prospect.jl
@@ -1,5 +1,5 @@
 # 
-# Parrot.jl/src/generatedata.jl
+# Parrot.jl/src/prospect.jl
 #
 # By Elijah Mathews (me@elijahmathews.com)
 # openpgp4fpr:ac1d3fb1e8a5eb7d14bd587b2932c725055a90d8
