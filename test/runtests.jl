0dac94dd69980ec8de08958c99f03707fb556074
Elijah Mathews <pgp@elijahmathews.com>
2020-11-26 17:25:54 -0600

    add a couple tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,13 +5,27 @@
 # openpgp4fpr:ac1d3fb1e8a5eb7d14bd587b2932c725055a90d8
 # 
 
-using Test, Documenter, Parrot
+using Test, Documenter, Parrot, Random
 
-@testset "Hello World" begin
-	message = "Hello world!"
-	@test message == "Hello world!"
+@testset "Layers" begin
 
-	x = 2 + 2
-	@test x == 4
+    @testset "Alsing" begin
+
+        Random.seed!(1)
+
+        a = Alsing(8,3)
+        r = rand(8)
+
+        @test typeof.([a.W, a.b, a.α, a.β]) == [
+            Array{Float32,2},
+            Array{Float32,1},
+            Array{Float32,1},
+            Array{Float32,1},
+        ]
+
+        @test a(r) ≈ [0.26666774962631395, 0.32477348838252235, 0.14232596416862664]
+
+    end
+
 end
 
